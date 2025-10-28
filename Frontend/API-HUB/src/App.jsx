db727bdb47b7c65e18a1102e0f434be6a8efa900
Thabo-Tlou <tlouthabo07@gmail.com>
2025-10-28 11:37:02 +0200

    commit

--- Frontend/API-HUB/src/App.jsx
+++ Frontend/API-HUB/src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import Documentation from './pages/Documentation'
 import Community from './pages/Community'
 import Products from './pages/Products'
+import SignIn from './components/SignIn'
+import Enterprise from './pages/Enterprise' // You'll need to create this component
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <Products />
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignIn />} /> {/* You can reuse SignIn or create a separate SignUp component */}
+        <Route path="/products" element={<Products />} />
+        <Route path="/documentation" element={<Documentation />} />
+        <Route path="/enterprise" element={<Enterprise />} />
+        <Route path="/community" element={<Community />} />
+        {/* Add a catch-all route for 404 pages */}
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </Router>
   )
 }
 
--- Frontend/API-HUB/src/components/Header.jsx
+++ Frontend/API-HUB/src/components/Header.jsx
@@ -1,23 +1,56 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Header.css";
-import logo from "../assets/API_HUB LOGO.png"
+import logo from "../assets/API_HUB LOGO.png";
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <header className="navbar">
-      <div className="logo">
-        <div className="logo-icon"><img src={logo} alt="API logo" className="logo" /></div>
+      <Link to="/" className="logo">
+        <div className="logo-icon">
+          <img src={logo} alt="API Hub logo" className="logo-img" />
+        </div>
         API Hub
-      </div>
+      </Link>
       <ul className="nav-links">
-        <li><span className="nav-number">01</span> Products</li>
-        <li><span className="nav-number">02</span> Documentation</li>
-        <li><span className="nav-number">03</span> Enterprise</li>
-        <li><span className="nav-number">04</span> Community</li>
+        <li>
+          <Link 
+            to="/products" 
+            className={location.pathname === "/products" ? "active" : ""}
+          >
+            <span className="nav-number">01</span> Products
+          </Link>
+        </li>
+        <li>
+          <Link 
+            to="/documentation" 
+            className={location.pathname === "/documentation" ? "active" : ""}
+          >
+            <span className="nav-number">02</span> Documentation
+          </Link>
+        </li>
+        <li>
+          <Link 
+            to="/enterprise" 
+            className={location.pathname === "/enterprise" ? "active" : ""}
+          >
+            <span className="nav-number">03</span> Enterprise
+          </Link>
+        </li>
+        <li>
+          <Link 
+            to="/community" 
+            className={location.pathname === "/community" ? "active" : ""}
+          >
+            <span className="nav-number">04</span> Community
+          </Link>
+        </li>
       </ul>
       <div className="nav-actions">
-        <button className="nav-btn login">Sign In</button>
-        <button className="nav-btn signup">Get Started</button>
+        <Link to="/signin" className="nav-btn login">Sign In</Link>
+        <Link to="/signup" className="nav-btn signup">Get Started</Link>
       </div>
     </header>
   );
