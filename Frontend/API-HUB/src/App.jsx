8e2f0002397836c33f65554959d18d2ed1050d33
Mamosa394 <motsiemamosa@gmail.com>
2025-10-28 11:14:49 -0700

    enterprise page

--- Frontend/API-HUB/src/App.jsx
+++ Frontend/API-HUB/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import LandingPage from './components/LandingPage'
+import Enterprise from './components/Enterprise'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <LandingPage />
+   
+     <Enterprise />
     </>
   )
 }
